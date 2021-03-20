c99b9a928749a7a19713fc2162faff3528f2105e
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-03-20 13:03:37 -0400

    added  movement

--- MiniJam76/MiniJam76/player.gd
+++ MiniJam76/MiniJam76/player.gd
@@ -1,10 +1,4 @@
 extends KinematicBody2D
 
-var rotation_point = Vector2(100, 100)
-var rotation_around_point = 0
-var distance_from_point = 100
-func _process(delta):
-		# set the position to the point
-	global_position = rotation_point
-		# offset using the rotation
-	global_position += Vector2(cos(rotation_around_point), sin(rotation_around_point)) * distance_from_point
+func _physics_process(delta):
+	look_at(get_global_mouse_position()) 
