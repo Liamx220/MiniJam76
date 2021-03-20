f7db9b101b84956709553b7e57472fd2743a5d71
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-03-20 19:38:17 -0400

    update

---
+++ MiniJam76/MiniJam76/pauseMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	if Input.is_action_pressed("pause"):
+		visible = true
+		get_tree().paused = true
+
+
+func _on_Button_pressed():
+	visible = false
+	get_tree().paused = false
