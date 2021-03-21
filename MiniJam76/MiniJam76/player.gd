6916d0a7f20f9a6ae96111913311be59f59cc6e6
soeyzandiego <59988129+soeyzandiego@users.noreply.github.com>
2021-03-20 23:09:36 -0500

    animated sunrays + added acceleration to satellite

--- MiniJam76/MiniJam76/player.gd
+++ MiniJam76/MiniJam76/player.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
 
-export (float) var rotation_speed = 4
+export (float) var max_rot_speed = 4
+export (float) var acceleration = 1
 
 onready var sprite = $Sprite
 var velocity = Vector2()
 var rotation_dir = 0
+var rotation_speed = 0
 
 
 func get_input():
@@ -15,7 +17,13 @@ func get_input():
 		rotation_dir += 1
 	if Input.is_action_pressed('left'):
 		rotation_dir -= 1 
-
+	
+	if (rotation_dir != 0):
+		rotation_speed += 0.25 * acceleration	
+	else:
+		rotation_speed = 0
+		
+	rotation_speed = clamp(rotation_speed, 0, max_rot_speed)
 
 func _physics_process(delta):
 	
