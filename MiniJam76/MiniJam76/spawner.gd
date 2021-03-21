4a7931ba7213d2eac7a936b043740ef21341078e
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-03-21 12:44:19 -0400

    added difficulty

--- MiniJam76/MiniJam76/spawner.gd
+++ MiniJam76/MiniJam76/spawner.gd
@@ -1,15 +1,19 @@
 extends Node
 const enemy = preload("res://enemy.tscn")
-
+var spawntimeMin = 1.0
+var spawntimeMax = 1.87
 
 
 func _ready():
+
 	spawn()
 	pass
 
 
 func spawn():
  while true:
+  spawntimeMin -= 0.01
+  spawntimeMax -= 0.01
   randomize()
   var enemy_in = enemy.instance()
   var pos = Vector2() 
@@ -17,5 +21,5 @@ func spawn():
   pos.y = rand_range(364.062, 18.994)
   enemy_in.set_position(pos)
   get_node("container").add_child(enemy_in)
-  yield(get_tree().create_timer(rand_range(1.0, 1.87)), "timeout") 
+  yield(get_tree().create_timer(rand_range(spawntimeMin, spawntimeMax)), "timeout") 
  pass
